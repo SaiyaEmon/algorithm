bed1935375ce73ddfd6db1bf20dbba6ace959665
SaiyaEmon <saiyaemon@aliyun.com>
2017-08-21 17:08:48 +0800

    改变数组变量的名字（主要为了试试commit能不能用）

--- src/main/java/com/saiyaemon/algorithm/sort/shellSort.java
+++ src/main/java/com/saiyaemon/algorithm/sort/shellSort.java
@@ -6,24 +6,24 @@ package com.saiyaemon.algorithm.sort;
 public class shellSort {
 
     public static void main(String[] args) {
-        int[] a = {52, 23, 18, 6, 11, 9, 7, 3, 14, 21, 42, 17, 32, 15};
-        int d = a.length;
+        int[] sorted = {52, 23, 18, 6, 11, 9, 7, 3, 14, 21, 42, 17, 32, 15};
+        int d = sorted.length;
         int temp;
         while (true) {
             d = (int) Math.ceil(d / 2);
             System.out.println(d);
             for (int x = 0; x < d; x++) {
-                for (int i = x + d; i < a.length; i += d) {
+                for (int i = x + d; i < sorted.length; i += d) {
                     int j = i - d;
-                    temp = a[i];
-                    for (; j >= 0 && temp < a[j]; j -= d) {
-                        a[j + d] = a[j];
+                    temp = sorted[i];
+                    for (; j >= 0 && temp < sorted[j]; j -= d) {
+                        sorted[j + d] = sorted[j];
                     }
-                    a[j + d] = temp;
+                    sorted[j + d] = temp;
                 }
             }
-            for (int i = 0; i < a.length; i++) {
-                System.out.print(a[i] + " ");
+            for (int i = 0; i < sorted.length; i++) {
+                System.out.print(sorted[i] + " ");
             }
             System.out.println();
             if (d == 1)
